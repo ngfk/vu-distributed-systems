77053c624abaa03d6925adb2047ec56345c30ec4
kishannirghin <kishannirghin@localhost>
2017-11-27 16:23:57 +0100

    another random commit

---
+++ ds-grid/src/distributed/systems/assignmentA/Scheduler.java
@@ -0,0 +1,19 @@
+package distributed.systems.assignmentA;
+
+
+/**
+ * A scheduler know where to find all resource managers, other schedulers, and users?
+ * 
+ * All scheduler communication should go through sockets 
+ *
+ */
+public class Scheduler {
+
+	private int id;
+	
+	
+	
+	Scheduler(int id){
+		this.id = id;
+	}
+}
--- ds-grid/src/distributed/systems/assignmentA/Simulation.java
+++ ds-grid/src/distributed/systems/assignmentA/Simulation.java
@@ -1,9 +1,26 @@
 package distributed.systems.assignmentA;
 
+import java.util.ArrayList;
+
 /**
  * This class should initialize every other class
  * and ultimately start the simulation
  */
 public class Simulation {
-
+	private static final int NUMBER_OF_SCHEDULERS = 5;
+	private static final int NUMBER_OF_RESOURCE_MANAGERS = 20;
+	private static final int NUMBER_OF_WORKER_NODES = 50; /* per resource manager */
+	
+	private ArrayList <Scheduler> schedulers;
+	
+	Simulation(){
+		/* create all instances */
+		for (int i = 0; i < NUMBER_OF_SCHEDULERS; i++) {
+			schedulers.add(new Scheduler(i));
+		}
+	}
+	
+	public static void main(String[] args) {
+		new Simulation();
+	}
 }
