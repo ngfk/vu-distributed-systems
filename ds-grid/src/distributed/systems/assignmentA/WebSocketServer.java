381061b82c153d1d3d24355a435610eb10cf45f3
Mitchell <mitchellwerensteijn@msn.com>
2017-11-30 18:44:55 +0100

    Switched to Jetty Websocket Server, receives and prints the register message

---
+++ ds-grid/src/distributed/systems/assignmentA/WebSocketServer.java
@@ -0,0 +1,37 @@
+package distributed.systems.assignmentA;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.websocket.server.WebSocketHandler;
+import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
+
+public class WebSocketServer implements Runnable {
+	/// Websocket server port
+	private static final int PORT = 4000;
+
+	/**
+	 * The WebsocketServer thread run method
+	 */
+	@Override
+	public void run() {
+		// Create WebSocket server.
+		Server server = new Server(PORT);
+		
+		// Register WebSocket callback.
+		WebSocketHandler wsHandler = new WebSocketHandler() {
+			@Override
+			public void configure(WebSocketServletFactory factory) {
+				factory.register(SimulationWebSocketHandler.class);
+			}
+		};
+		server.setHandler(wsHandler);
+		
+		// Start WebSocket server.
+		try {
+			server.start();
+			server.join();
+		} catch (Exception e) {
+			System.out.println("ERROR: Can't start Websocket Server, port " + PORT + " in use?");
+		}
+	}
+
+}
