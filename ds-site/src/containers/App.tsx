366294bbedfa55cac538ea93d124dd7109e04b08
Arthur van der Weiden <arthurweiden1@gmail.com>
2017-12-02 10:20:16 +0100

    App update

--- ds-site/src/containers/App.tsx
+++ ds-site/src/containers/App.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Cluster } from '../components/Cluster';
-import { Scheduler } from '../components/Scheduler';
+import { SchedulerCluster } from '../components/SchedulerCluster';
 import { User } from '../components/User';
 import { State } from '../reducers/reducer';
 import { actionCreators, ActionCreators } from '../actions/actions';
@@ -18,82 +18,44 @@ export interface AppProps {
 }
 
 export interface AppState {
-    active: number;
-    randomResource: number;
-    randomCluster: number;
-    randomWorker: number;
-    randomScheduler: number;
+    clusterCount: number;
+    workerCount: number;
+    schedulerCount: number;
 }
 
 class App extends React.Component<AppProps, AppState> {
     constructor(props: AppProps) {
         super(props);
-        this.state = {
-            active: 0,
-            randomWorker: 0,
-            randomCluster: 0,
-            randomResource: 0,
-            randomScheduler: 0
-        };
+        this.state = { clusterCount: 0, workerCount: 0, schedulerCount: 0 };
+    }
 
-        // setInterval(() => {
-        //     this.setState(state => ({
-        //         ...state,
-        //         active: (this.state.active + 1) % 3,
-        //         randomCluster: Math.floor(Math.random() * 20),
-        //         randomResource: Math.floor(Math.random() * 20),
-        //         randomWorker: Math.floor(Math.random() * 50),
-        //         randomScheduler: Math.floor(Math.random() * 10)
-        //     }));
-        // }, 500);
+    // private getClusterCount = () => {
+    //     return this.state.clusterCount.toString();
+    // };
+    private handleChangeCluster(e: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({ clusterCount: parseInt(e.target.value) });
+    }
+
+    private handleChangeWorker(e: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({ workerCount: parseInt(e.target.value) });
+    }
+
+    private handleChangeScheduler(e: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({ schedulerCount: parseInt(e.target.value) });
     }
 
     public render(): JSX.Element {
         let clusters = [];
 
-        for (let i = 1; i < 10; i++) {
-            if (i === this.state.randomResource)
-                clusters.push(
-                    <Cluster
-                        workers={50}
-                        key={'ResourceDown' + i.toString()}
-                        resourceActive={1}
-                        workerActive={(this.state.active + 3) % 3}
-                        workerNode={51}
-                    />
-                );
-            else
-                clusters.push(
-                    <Cluster
-                        workers={50}
-                        key={'ResourceUp' + i.toString()}
-                        resourceActive={2}
-                        workerActive={(this.state.active + 3) % 3}
-                        workerNode={51}
-                    />
-                );
-
-            if (i === this.state.randomCluster)
-                clusters.push(
-                    <Cluster
-                        workers={50}
-                        key={'WorkerDown' + i.toString()}
-                        resourceActive={2}
-                        workerActive={(this.state.active + 3) % 3}
-                        workerNode={this.state.randomWorker}
-                    />
-                );
-            else
-                clusters.push(
-                    <Cluster
-                        workers={50}
-                        key={'WorkerUp' + i.toString()}
-                        resourceActive={2}
-                        workerActive={(this.state.active + 3) % 3}
-                        workerNode={51}
-                    />
-                );
+        for (let i = 0; i < this.state.clusterCount; i++) {
+            clusters.push(
+                <Cluster
+                    workers={this.state.workerCount}
+                    key={'ResourceDown' + i.toString()}
+                />
+            );
         }
+
         return (
             <div className="App">
                 <div className="App-header">
@@ -101,16 +63,31 @@ class App extends React.Component<AppProps, AppState> {
                     <h2>Welcome to Ludwig Dahl Fanclub</h2>
                 </div>
                 <h1 className="App-intro">Simulation of the grid schedulers</h1>
-
+                <div className="inputForm">
+                    <label>Clusters </label>
+                    <input
+                        type="text"
+                        name="clusterCount"
+                        onChange={this.handleChangeCluster.bind(this)}
+                    />
+                    <label>Workers </label>
+                    <input
+                        type="text"
+                        name="workerCount"
+                        onChange={this.handleChangeWorker.bind(this)}
+                    />
+                    <label>Schedulers </label>
+                    <input
+                        type="text"
+                        name="schedulerCount"
+                        onChange={this.handleChangeScheduler.bind(this)}
+                    />
+                </div>
                 <div className="App-body">
                     <div className="User-box">
                         <User />
                     </div>
-                    <Scheduler
-                        schedulers={5}
-                        active={1}
-                        randomScheduler={this.state.randomScheduler}
-                    />
+                    <SchedulerCluster schedulers={this.state.schedulerCount} />
                     <div className="Cluster-box"> {clusters} </div>
                 </div>
             </div>
