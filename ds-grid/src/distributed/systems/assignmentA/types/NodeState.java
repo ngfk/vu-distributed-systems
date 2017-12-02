4f6e39bf606f402f1cc530894f4f0ee67ce424d2
Mitchell <mitchellwerensteijn@msn.com>
2017-12-02 18:21:17 +0100

    Change GridMessageState object for GSON

--- ds-grid/src/distributed/systems/assignmentA/types/NodeState.java
+++ ds-grid/src/distributed/systems/assignmentA/types/NodeState.java
@@ -1,8 +1,14 @@
 package distributed.systems.assignmentA.types;
 
+import com.google.gson.annotations.SerializedName;
+
 public enum NodeState {
+	@SerializedName("online")
 	ONLINE,
+	@SerializedName("offline")
 	OFFLINE,
+	@SerializedName("busy")
 	BUSY,
+	@SerializedName("unreachable")
 	UNREACHABLE
 }
