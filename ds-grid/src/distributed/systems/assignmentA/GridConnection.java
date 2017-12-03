b6e95c5a0a7591bf2af0f4202b1229fb99255517
Rick Koster <ngf.koster@outlook.com>
2017-12-03 10:01:22 +0100

    refactor(ds-grid): remove unused class

--- ds-grid/src/distributed/systems/assignmentA/GridConnection.java
+++
@@ -1,8 +0,0 @@
-package distributed.systems.assignmentA;
-
-import org.eclipse.jetty.websocket.api.Session;
-
-public class GridConnection {
-	public Session session;
-	public Simulation simulation;
-}
