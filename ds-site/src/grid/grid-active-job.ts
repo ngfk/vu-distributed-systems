bff9cc62a913370c4ad6adb4664b378c90f82340
Arthur van der Weiden <arthurweiden1@gmail.com>
2017-12-06 00:18:35 +0100

    activejob

--- ds-site/src/grid/grid-active-job.ts
+++ ds-site/src/grid/grid-active-job.ts
@@ -1,4 +1,5 @@
-import { GridJob } from './grid-job';
+import { GridJob, JobStatus } from './grid-job';
+import { GridSocket } from './grid-socket';
 
 export enum ActiveJobStatus {
     Unconfirmed,
@@ -7,5 +8,43 @@ export enum ActiveJobStatus {
 }
 
 export class GridActiveJob {
-    constructor(private job: GridJob) {}
+    private schedulers = new Map<GridSocket, ActiveJobStatus>();
+
+    constructor(
+        public job: GridJob,
+        public status: JobStatus,
+        public workerSocket: GridSocket,
+        public rmSocket: GridSocket,
+        private schedulerSockets: GridSocket[]
+    ) {
+        if (schedulerSockets !== null) {
+            schedulerSockets.forEach(socket => {
+                this.schedulers.set(socket, ActiveJobStatus.Unconfirmed);
+            });
+        }
+    }
+
+    public confirmScheduler(scheduler: GridSocket): void {
+        this.schedulers.set(scheduler, ActiveJobStatus.Confirmed);
+    }
+
+    public markAsDone(scheduler: GridSocket): void {
+        this.schedulers.set(scheduler, ActiveJobStatus.Done);
+    }
+
+    public canStart(): boolean {
+        const statuses = [...this.schedulers.values()];
+
+        for (let i = 0; i < statuses.length; i++) {
+            if (statuses[i] === ActiveJobStatus.Unconfirmed) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public isFinished(): boolean {
+        const statuses = [...this.schedulers.values()];
+        return statuses.some(s => s === ActiveJobStatus.Done);
+    }
 }
